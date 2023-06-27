2e0c40e3758bc33b3f5dfb62c6e48f393328e121
yz4004 <yz4004@columbia.edu>
2023-06-26 19:54:17 -0700

    initial project sturcture

---
+++ src/main/java/com/example/graphql_demo/GraphQlDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.graphql_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphQlDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphQlDemoApplication.class, args);
+    }
+
+}
